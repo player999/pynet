c3fea74042d7b54003870650b8a539df49859f29
player999 <taras.zakharchenko@gmail.com>
2015-11-08 00:31:27 +0200

    Configs in separate file

--- pynet/__init__.py
+++ pynet/__init__.py
@@ -5,13 +5,7 @@ import skimage.io
 import skimage.color
 
 #CONFIGURATION
-
-#Config for features of GOOGLENET
-MODEL_FILE = '/home/player999/Work/VehicleClassification/deploy_features.prototxt'
-PRETRAINED = '/home/player999/Work/VehicleClassification/GoogLenet/bvlc_googlenet.caffemodel'
-MEAN_FILE = '/home/player999/Work/digits/caffe/python/caffe/imagenet/ilsvrc_2012_mean.npy'
-CAFFE_ROOT = '/home/player999/Work/digits/caffe'
-#LABEL_FILE = '/home/player999/Work/VehicleClassification/our_net/labels.txt'
+from pynet_config import *
 
 IMAGE_WIDTH = 224
 IMAGE_HEIGHT = 224
---
+++ pynet/pynet_config.py
@@ -0,0 +1,6 @@
+#Config for features of GOOGLENET
+MODEL_FILE = '/home/player999/Work/VehicleClassification/deploy_features.prototxt'
+PRETRAINED = '/home/player999/Work/VehicleClassification/GoogLenet/bvlc_googlenet.caffemodel'
+MEAN_FILE = '/home/player999/Work/digits/caffe/python/caffe/imagenet/ilsvrc_2012_mean.npy'
+CAFFE_ROOT = '/home/player999/Work/digits/caffe'
+#LABEL_FILE = '/home/player999/Work/VehicleClassification/our_net/labels.txt'
