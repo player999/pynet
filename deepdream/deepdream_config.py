05d1399409334e50487371be6d9bba8743507e72
player999 <taras.zakharchenko@gmail.com>
2015-11-08 07:09:15 +0200

    Deep dream

---
+++ deepdream/deepdream_config.py
@@ -0,0 +1,3 @@
+CAFFE_ROOT = '/home/player999/Work/digits/caffe'
+MODEL_FILE = '/home/player999/Work/VehicleClassification/deploy_dream.prototxt'
+PRETRAINED = '/home/player999/Work/VehicleClassification/our_net16/snapshot_iter_24000.caffemodel'
